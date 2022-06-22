f622c3ce0b6ef67b7fe56494404ba8a69d6976f3
Anthony Chen <88166446+tonyechen@users.noreply.github.com>
2022-06-22 17:44:05 -0400

    Sidebar update

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -24,9 +24,7 @@ const Sidebar = () => {
                     <LibraryIcon className="h-5 w-5" />
                     <p>Your Library</p>
                 </button>
-
                 <hr className="border-t-[0.1px] border-gray-900" />
-
                 <button className="flex items-center space-x-2 hover:text-white">
                     <PlusCircleIcon className="h-5 w-5" />
                     <p>Create PlayList</p>
@@ -39,8 +37,32 @@ const Sidebar = () => {
                     <RssIcon className="h-5 w-5" />
                     <p>Your Library</p>
                 </button>
-
                 <hr className="border-t-[0.1px] border-gray-900" />
+                {/*Playlists... */}
+                <p className="cursor-pointer hover:text-white">
+                    Playlist name...
+                </p>
+                <p className="cursor-pointer hover:text-white">
+                    Playlist name...
+                </p>{' '}
+                <p className="cursor-pointer hover:text-white">
+                    Playlist name...
+                </p>{' '}
+                <p className="cursor-pointer hover:text-white">
+                    Playlist name...
+                </p>{' '}
+                <p className="cursor-pointer hover:text-white">
+                    Playlist name...
+                </p>{' '}
+                <p className="cursor-pointer hover:text-white">
+                    Playlist name...
+                </p>{' '}
+                <p className="cursor-pointer hover:text-white">
+                    Playlist name...
+                </p>{' '}
+                <p className="cursor-pointer hover:text-white">
+                    Playlist name...
+                </p>
             </div>
         </div>
     );
